2d5ea73e2cd30ba5ee1ce7de2abd5f3f7cbdf98f
victorylum <lumtegha@email.com>
2025-02-22 21:10:07 +0200

    second

---
+++ Animal.java
@@ -0,0 +1,18 @@
+public class Animal{
+static String name;
+static String species;
+       int age;
+       double wieght;
+       int lifeSpan;
+       double heartRate;
+       double avgTemperature;
+public void sound(){
+
+}
+
+public static void Main(String[]args){
+Animal a=new Animal();
+
+   }
+}
+
