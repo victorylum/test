1abeadaf011bd0faaff0b4379cf9f119b89e8772
victorylum <lumtegha@email.com>
2025-02-24 15:45:47 +0200

    first commit

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee{
+String name;
+int age;
+int contact;
+String address;
+String position;
+static int salary;
+public void ticket(){
+
+}
+
+public static void main(String[]args){
+Employee e=new Employee();
+
+  }
+}
