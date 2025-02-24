d60d7cc6e3eda103159658942e02ae4390abda4f
victorylum <lumtegha@gmail.com>
2025-02-24 16:24:31 +0200

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -6,13 +6,18 @@ static String species;
        int lifeSpan;
        double heartRate;
        double avgTemperature;
-public void sound(){
+public void sound(it make's sound){
 
 }
 
 public static void Main(String[]args){
 Animal a=new Animal();
-
+String name=Lion;
+String species=Panthera leo;
+a.age=20;
+   int a=2;
+       a.sound(it make's sound);
+       
    }
 }
 
